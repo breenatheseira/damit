739ce40dcfae472e57a09b6d46895541fcb06c88
Breena Theseira <breenatheseira@yahoo.com>
2015-05-26 02:14:48 +0800

    Fixed: Accurate results are obtained from the loop

--- R/damcat.R
+++ R/damcat.R
@@ -7,6 +7,8 @@ library("rpart.plot")
 library("ggplot2")
 
 dam_prediction <- get(load('./data/River.rda'))
+sample_data <- data.frame(get(load('./data/DailyValues.rda')))
+sample_data$Wind<- recode(sample_data$Wind, "FALSE='False'; TRUE ='True'")
 
 fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rain,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
 
@@ -23,23 +25,18 @@ damcategory <- function(daySelect = "1", monthSelect = "January", riverSelect = 
     Rain =as.numeric(rainSelect),
     Wind = windSelect)
 
-  return(result = predict(fit,newdata=newdata,type=c("class")))
+  return(predict(fit,newdata=newdata,type=c("class")))
 }
 
-new_date <- function(date = "1 January", duration){
+new_date <- function(day = "1", month ="January"){
 
   monthArr <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
   daysArr <- c(31,28,31,30,31,30,31,31,30,31,30,31)
 
-  date <- unlist(strsplit(date, " ", fixed = TRUE))
-  day = date[1]
-  month = date[2]
-
-  total_days <- as.numeric(day) + as.numeric(duration)
-
+  total_days <- as.numeric(day)
   for (i in 1:length(monthArr)){
     if (month == monthArr[i]){
-      if (total_days < daysArr[i]){
+      if (total_days <= daysArr[i]){
         new_day <- total_days
         new_month <- month
       } else {
@@ -55,30 +52,26 @@ new_date <- function(date = "1 January", duration){
   return(paste(new_day, new_month, sep = " "))
 }
 
-river_result <- function(date = "1 January", river = "Sungai Galas, Dabong", rain = "1", wind = "True"){
+river_result <- function(day = "1", month ="January", river = "Sungai Galas, Dabong", rain = "1", wind = "True"){
+
   # today's result
+  date <- new_date(day, month);
   split <- unlist(strsplit(date, " ", fixed = TRUE))
-  today <- damcategory(split[1],split[2],river, rain, wind)
+  today <- damcategory(day,month,river, rain, wind)
+  return(today)
+}
 
-  # tomorrow
-  date <- new_date(date, 1);
-  split <- unlist(strsplit(date, " ", fixed = TRUE))
-  tomorrow <- damcategory(split[1],split[2],river, rain, wind)
+bundled_result <- function(){
+  array <- NULL
 
-  # after 1 week
-  date <- new_date(date, 7);
-  split <- unlist(strsplit(date, " ", fixed = TRUE))
-  after1_week <- damcategory(split[1],split[2],river, rain, wind)
+  for (i in 1:30){
+    today <- river_result(sample_data[i,1],sample_data[i,2],"Sungai Galas, Dabong",sample_data[i,3],sample_data[i,4])
+    tomorrow <- river_result(sample_data[i+1,1],sample_data[i+1,2],"Sungai Galas, Dabong",sample_data[i+1,3],sample_data[i+1,4])
+    week1 <- river_result(sample_data[i+7,1],sample_data[i+7,2],"Sungai Galas, Dabong",sample_data[i+7,3],sample_data[i+7,4])
+    week2 <- river_result(sample_data[i+14,1],sample_data[i+14,2],"Sungai Galas, Dabong",sample_data[i+14,3],sample_data[i+14,4])
+    month <- river_result(sample_data[i+30,1],sample_data[i+30,2],"Sungai Galas, Dabong",sample_data[i+30,3],sample_data[i+30,4])
+    array[i] <- paste(today, tomorrow, week1, week2, month, sep = " ")
+  }
 
-  # after 1 fortnight
-  date <- new_date(date, 14);
-  split <- unlist(strsplit(date, " ", fixed = TRUE))
-  after1_week <- damcategory(split[1],split[2],river, rain, wind)
-
-  # after 1 month
-  date <- new_date(date, 30);
-  split <- unlist(strsplit(date, " ", fixed = TRUE))
-  after_1month <- damcategory(split[1],split[2],river, rain, wind)
-
-  return(paste(today, tomorrow, after1_week, after1_week, after_1month))
+  return(array)
 }
