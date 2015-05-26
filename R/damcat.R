a6e9b3fe635c5cfec7d98af6ccce07827576a5bb
Breena Theseira <breenatheseira@yahoo.com>
2015-05-26 11:27:52 +0800

    Committed strange code

--- R/damcat.R
+++ R/damcat.R
@@ -7,8 +7,8 @@ library("rpart.plot")
 library("ggplot2")
 
 dam_prediction <- get(load('./data/River.rda'))
-sample_data <- data.frame(get(load('./data/DailyValues.rda')))
-sample_data$Wind<- recode(sample_data$Wind, "FALSE='False'; TRUE ='True'")
+sample_df <- get(load('./data/DailyValues.rda'))
+sample_data <- data.frame(sample_df)
 
 fit <- rpart(Rank_Dam ~ Day + Month + River + Wind + Rain,method="class", data=dam_prediction,control=rpart.control(minsplit=4))
 
@@ -28,48 +28,50 @@ damcategory <- function(daySelect = "1", monthSelect = "January", riverSelect = 
   return(predict(fit,newdata=newdata,type=c("class")))
 }
 
-new_date <- function(day = "1", month ="January"){
-
-  monthArr <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
-  daysArr <- c(31,28,31,30,31,30,31,31,30,31,30,31)
-
-  total_days <- as.numeric(day)
-  for (i in 1:length(monthArr)){
-    if (month == monthArr[i]){
-      if (total_days <= daysArr[i]){
-        new_day <- total_days
-        new_month <- month
-      } else {
-        new_day <- total_days - daysArr[i]
-        # for Dec to return to January
-        if (month == "December")
-          new_month <- monthArr[1]
-        else
-          new_month <- monthArr[i+1]
-      }
-    }
-  }
-  return(paste(new_day, new_month, sep = " "))
-}
-
-river_result <- function(day = "1", month ="January", river = "Sungai Galas, Dabong", rain = "1", wind = "True"){
-
-  # today's result
-  date <- new_date(day, month);
-  split <- unlist(strsplit(date, " ", fixed = TRUE))
-  today <- damcategory(day,month,river, rain, wind)
-  return(today)
-}
+# new_date <- function(day = "1", month ="January"){
+#
+#   monthArr <- c("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+#   daysArr <- c(31,28,31,30,31,30,31,31,30,31,30,31)
+#
+#   total_days <- as.numeric(day)
+#   for (i in 1:length(monthArr)){
+#     if (month == monthArr[i]){
+#       if (total_days <= daysArr[i]){
+#         new_day <- total_days
+#         new_month <- month
+#       } else {
+#         new_day <- total_days - daysArr[i]
+#         # for Dec to return to January
+#         if (month == "December")
+#           new_month <- monthArr[1]
+#         else
+#           new_month <- monthArr[i+1]
+#       }
+#     }
+#   }
+#   return(paste(new_day, new_month, sep = " "))
+# }
+#
+# river_result <- function(day = "1", month ="January", river = "Sungai Galas, Dabong", rain = "1", wind = "True"){
+#
+#   # today's result
+#   date <- new_date(day, month);
+#   split <- unlist(strsplit(date, " ", fixed = TRUE))
+#   today <- damcategory(day,month,river, rain, wind)
+#   return(today)
+# }
 
 bundled_result <- function(){
-  array <- NULL
 
+  sample_data$Wind<- recode(sample_data$Wind, "FALSE='False'; TRUE ='True'")
+
+  array <- NULL
   for (i in 1:30){
-    today <- river_result(sample_data[i,1],sample_data[i,2],"Sungai Galas, Dabong",sample_data[i,3],sample_data[i,4])
-    tomorrow <- river_result(sample_data[i+1,1],sample_data[i+1,2],"Sungai Galas, Dabong",sample_data[i+1,3],sample_data[i+1,4])
-    week1 <- river_result(sample_data[i+7,1],sample_data[i+7,2],"Sungai Galas, Dabong",sample_data[i+7,3],sample_data[i+7,4])
-    week2 <- river_result(sample_data[i+14,1],sample_data[i+14,2],"Sungai Galas, Dabong",sample_data[i+14,3],sample_data[i+14,4])
-    month <- river_result(sample_data[i+30,1],sample_data[i+30,2],"Sungai Galas, Dabong",sample_data[i+30,3],sample_data[i+30,4])
+    today <- damcategory(sample_data[i,1],sample_data[i,2],"Sungai Galas, Dabong",sample_data[i,3],sample_data[i,4])
+    tomorrow <- damcategory(sample_data[i+1,1],sample_data[i+1,2],"Sungai Galas, Dabong",sample_data[i+1,3],sample_data[i+1,4])
+    week1 <- damcategory(sample_data[i+7,1],sample_data[i+7,2],"Sungai Galas, Dabong",sample_data[i+7,3],sample_data[i+7,4])
+    week2 <- damcategory(sample_data[i+14,1],sample_data[i+14,2],"Sungai Galas, Dabong",sample_data[i+14,3],sample_data[i+14,4])
+    month <- damcategory(sample_data[i+30,1],sample_data[i+30,2],"Sungai Galas, Dabong",sample_data[i+30,3],sample_data[i+30,4])
     array[i] <- paste(today, tomorrow, week1, week2, month, sep = " ")
   }
 
---
+++ man/DailyValues.rd
@@ -0,0 +1,25 @@
+\name{DailyValues}
+\alias{DailyValues}
+\docType{data}
+\title{
+Sample Rain and Wind Data
+}
+\description{
+Sample data from Nov 15 to Jan 13 for obtaining rain values
+}
+\usage{data("DailyValues")}
+\format{
+  A data frame with 60 observations on the following 4 variables.
+  \describe{
+    \item{\code{Day}}{a string vector}
+    \item{\code{Month}}{a string vector}
+    \item{\code{Rain}}{a numeric vector}
+    \item{\code{Wind}}{a string vector}
+  }
+}
+\value{
+  dataframe 'sample_data'
+}
+\arguments{
+  \item{max}{Max number of data to return}
+}
